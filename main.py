b358e6ed68e239c7f88103eef785a9682e2b0553
SeungWoon Lee <s3ungw00n@gmail.com>
2024-03-21 09:47:05 +0900

    Apply mutex on handlers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from logger import log
 from celery import Celery
 from utils import LimitedSizeDict, strip_prefix_from_event
@@ -28,60 +29,64 @@ tasks_captured.set_function(lambda: len(queued_tasks))
 execution_stats = Summary('celery_tasks_duration_seconds',
                           'Duration of tasks when finished in given state', ['queue', 'task_name', 'worker', 'state'])
 
+mutex = threading.Lock()
 
 # this monitor captures celery events
 def my_monitor(app):
 
     # event: task entered the queue
     def get_sent_time(event):
-        task_id = event['uuid']
-        # timestamp, queue and taskname are only send at the sent-event
-        queued_tasks[task_id] = {
-            "ts": event['timestamp'],
-            "queue": event['routing_key'],
-            "taskname": event['name']
-        }
+        with mutex:
+            task_id = event['uuid']
+            # timestamp, queue and taskname are only send at the sent-event
+            queued_tasks[task_id] = {
+                "ts": event['timestamp'],
+                "queue": event['routing_key'],
+                "taskname": event['name']
+            }
 
-        # This works because queued_tasks is a LimitedSizeDict which could possibly "reject" new keys being added.
-        # Therefore we check if the key is stored so the information about this task is available in later steps.
-        if task_id in queued_tasks:
-            # only consider task in metrics if it has been stored in the dict
-            queue_length.labels(queue=event['routing_key'], task_name=event['name']).inc()
+            # This works because queued_tasks is a LimitedSizeDict which could possibly "reject" new keys being added.
+            # Therefore we check if the key is stored so the information about this task is available in later steps.
+            if task_id in queued_tasks:
+                # only consider task in metrics if it has been stored in the dict
+                queue_length.labels(queue=event['routing_key'], task_name=event['name']).inc()
 
     def get_started_time(event):
-        task_id = event['uuid']
-        # check if tasks sent-event was captured previously
-        if task_id in queued_tasks:
-            # calculate waiting time in queue
-            lag = event['timestamp'] - queued_tasks[task_id]['ts']
-            # we have to use the information out of the dictionary,
-            # because only the task-sent event stores queue information
-            waiting_time.labels(queue=queued_tasks[task_id]['queue'],
-                                task_name=queued_tasks[task_id]['taskname']).observe(lag)
-            queue_length.labels(queue=queued_tasks[task_id]['queue'],
-                                task_name=queued_tasks[task_id]['taskname']).dec()
-            tasks_running.labels(queue=queued_tasks[task_id]['queue'],
-                                 task_name=queued_tasks[task_id]['taskname'],
-                                 worker=event['hostname']).inc()
-            # set ts to timestamp of event for calculation of duration in last step
-            queued_tasks[task_id]['ts'] = event['timestamp']
+        with mutex:
+            task_id = event['uuid']
+            # check if tasks sent-event was captured previously
+            if task_id in queued_tasks:
+                # calculate waiting time in queue
+                lag = event['timestamp'] - queued_tasks[task_id]['ts']
+                # we have to use the information out of the dictionary,
+                # because only the task-sent event stores queue information
+                waiting_time.labels(queue=queued_tasks[task_id]['queue'],
+                                    task_name=queued_tasks[task_id]['taskname']).observe(lag)
+                queue_length.labels(queue=queued_tasks[task_id]['queue'],
+                                    task_name=queued_tasks[task_id]['taskname']).dec()
+                tasks_running.labels(queue=queued_tasks[task_id]['queue'],
+                                     task_name=queued_tasks[task_id]['taskname'],
+                                     worker=event['hostname']).inc()
+                # set ts to timestamp of event for calculation of duration in last step
+                queued_tasks[task_id]['ts'] = event['timestamp']
 
     def get_task_done(event):
-        task_id = event['uuid']
-        # check if tasks sent-event was captured previously
-        if task_id in queued_tasks:
-            # we have to use the information out of the dictionary,
-            # because only the task-sent event stores queue information
-            tasks_running.labels(queue=queued_tasks[task_id]['queue'],
-                                 task_name=queued_tasks[task_id]['taskname'],
-                                 worker=event['hostname']).dec()
-            # now add statistics of task execution to metrics
-            runtime = event['timestamp'] - queued_tasks[task_id]['ts']
-            execution_stats.labels(queue=queued_tasks[task_id]['queue'],
-                                   task_name=queued_tasks[task_id]['taskname'],
-                                   worker=event['hostname'],
-                                   state=strip_prefix_from_event(event['type'])).observe(runtime)
-            queued_tasks.pop(task_id, None)
+        with mutex:
+            task_id = event['uuid']
+            # check if tasks sent-event was captured previously
+            if task_id in queued_tasks:
+                # we have to use the information out of the dictionary,
+                # because only the task-sent event stores queue information
+                tasks_running.labels(queue=queued_tasks[task_id]['queue'],
+                                     task_name=queued_tasks[task_id]['taskname'],
+                                     worker=event['hostname']).dec()
+                # now add statistics of task execution to metrics
+                runtime = event['timestamp'] - queued_tasks[task_id]['ts']
+                execution_stats.labels(queue=queued_tasks[task_id]['queue'],
+                                       task_name=queued_tasks[task_id]['taskname'],
+                                       worker=event['hostname'],
+                                       state=strip_prefix_from_event(event['type'])).observe(runtime)
+                queued_tasks.pop(task_id, None)
 
     # register handler functions for task events in the receiver configuration
     with app.connection() as connection:
